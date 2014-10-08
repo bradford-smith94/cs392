1146f2258ac2d992279f4ac55d7ec7db0ac773df
Bradford Smith <bsmith8@stevens.edu>
2014-10-07 20:42:44 -0400

    Added a usage message if called with no args

--- src/mypipes/main.c
+++ src/mypipes/main.c
@@ -19,62 +19,67 @@ int main(int argc, char **argv)
 	int n;
 	char *message;
 
-	if(pipe(pipe1))
-		my_err("ERROR failed to create pipe1!");
-	if((pid = fork()) < 0)
-		my_err("ERROR spawning DAD process!");
-	else if(pid > 0)
-	{
-		/*this is GRANDPA after spawning DAD*/
-		if(close(pipe1[0]))
-			my_err("ERROR failed to close pipe1[0] in GRANDPA process!");
-		message = my_vect2str(&argv[1]);
-		if(my_strlen(message) == -1){
-			free(message);
-			message = my_strdup("NULL");
-		}
-		my_str("This is Grandpa sending \"");
-		my_str(message);
-		my_str("\"\n");
-		if((write(pipe1[1], message, 256)) < 0)
-			my_err("ERROR failed to write to pipe1[1] in GRANDPA process!");
-		wait();
-	}
+	if(argc < 2)
+		my_err("usage: ./pipes arg1 [arg2] [arg3]...\n");
 	else
 	{
-		/*this is DAD*/
-		if(close(pipe1[1]))
-			my_err("ERROR failed to close pipe1[1] in DAD process!");
-		if(pipe(pipe2))
-			my_err("ERROR failed to create pipe2!");
+		if(pipe(pipe1))
+			my_err("ERROR failed to create pipe1!\n");
 		if((pid = fork()) < 0)
-			my_err("ERROR spawning CHILD process!");
+			my_err("ERROR spawning DAD process!\n");
 		else if(pid > 0)
 		{
-			/*this is DAD after spawning CHILD*/
-			if(close(pipe2[0]))
-				my_err("ERROR failed to close pipe2[0] in DAD process!");
-			n = read(pipe1[0], message, 256);
-			if(n < 0)
-				my_err("ERROR failed to read from pipe1[0] in DAD process!");
-			my_str("This is Dad recieving and sending \"");
+			/*this is GRANDPA after spawning DAD*/
+			if(close(pipe1[0]))
+				my_err("ERROR failed to close pipe1[0] in GRANDPA process!\n");
+			message = my_vect2str(&argv[1]);
+			if(my_strlen(message) == -1){
+				free(message);
+				message = my_strdup("NULL");
+			}
+			my_str("This is Grandpa sending \"");
 			my_str(message);
 			my_str("\"\n");
-			if((write(pipe2[1], message, 256)) < 0)
-				my_err("ERROR failed to write to pipe2[1] in DAD process!");
+			if((write(pipe1[1], message, 256)) < 0)
+				my_err("ERROR failed to write to pipe1[1] in GRANDPA process!\n");
 			wait();
 		}
 		else
 		{
-			/*this is CHILD*/
-			if(close(pipe2[1]))
-				my_err("ERROR failed to close pipe2[1] in CHILD process!");
-			n = read(pipe2[0], message, 256);
-			if(n < 0)
-				my_err("ERROR failed reading from pipe2[0] in CHILD process!");
-			my_str("This is Child recieving \"");
-			my_str(message);
-			my_str("\"\n");
+			/*this is DAD*/
+			if(close(pipe1[1]))
+				my_err("ERROR failed to close pipe1[1] in DAD process!\n");
+			if(pipe(pipe2))
+				my_err("ERROR failed to create pipe2!\n");
+			if((pid = fork()) < 0)
+				my_err("ERROR spawning CHILD process!\n");
+			else if(pid > 0)
+			{
+				/*this is DAD after spawning CHILD*/
+				if(close(pipe2[0]))
+					my_err("ERROR failed to close pipe2[0] in DAD process!\n");
+				n = read(pipe1[0], message, 256);
+				if(n < 0)
+					my_err("ERROR failed to read from pipe1[0] in DAD process!\n");
+				my_str("This is Dad recieving and sending \"");
+				my_str(message);
+				my_str("\"\n");
+				if((write(pipe2[1], message, 256)) < 0)
+					my_err("ERROR failed to write to pipe2[1] in DAD process!\n");
+				wait();
+			}
+			else
+			{
+				/*this is CHILD*/
+				if(close(pipe2[1]))
+					my_err("ERROR failed to close pipe2[1] in CHILD process!\n");
+				n = read(pipe2[0], message, 256);
+				if(n < 0)
+					my_err("ERROR failed reading from pipe2[0] in CHILD process!\n");
+				my_str("This is Child recieving \"");
+				my_str(message);
+				my_str("\"\n");
+			}
 		}
 	}
 	return 0;
