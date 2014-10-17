3287f5b8aa348cadd39e53546544e216a9e5c4e1
Bradford Smith <bsmith8@stevens.edu>
2014-10-16 20:28:00 -0400

    Worked on my_atoi.c

---
+++ src/my/my_atoi.c
@@ -0,0 +1,29 @@
+/*Bradford Smith (bsmith8)
+*my_atoi.c mysignals project
+*10/16/2014
+*"I pledge my honor that I have abided by the Stevens Honor System."
+*/
+
+#include "my.h"
+
+/*pre: takes in a char *s
+*post: returns the integer represented in s or 0 if there is no integer in s
+*/
+int my_atoi(char *s)
+{
+	int r;/*return value*/
+	int n;/*negative flag*/
+
+	r = 0;
+	c = 0;
+	if(s != NULL)
+	{
+		for( ; *s == ' ' || *s == '\t'; s++)
+			;/*skipping over leading whitespaces*/
+		for( ; *s == '+' || *s == '-'; s++)
+			(*s == '-') ? n ^= 1 : n;/*n is set to 1 for odd numbers of -'s*/
+		for( ; *s != '\0'; s++)
+			;/*read number*/
+	}
+	return r;
+}
