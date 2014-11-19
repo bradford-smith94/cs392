7c0f0a872ea111e595608d21bfea0800cc00abda
Bradford Smith <bsmith8@stevens.edu>
2014-11-18 23:44:36 -0500

    Made the popup windows static so that they only display once, still need to hook up connecting and sending messages

--- src/gtk_project_new_with_sockets/connect_cb.c
+++ src/gtk_project_new_with_sockets/connect_cb.c
@@ -6,55 +6,68 @@
 
 #include "mygtksockets.h"
 
-/*pre:
+/*pre: takes in a GtkWidget *w, and gpointer data
 *post: produces a pop-up window
 *	asks for server information then establishes a connection
 */
 void connect_cb(GtkWidget *w, gpointer data)
-{
-	GtkWidget *win;
-	GtkWidget *tab;
-	GtkWidget *b;
+{	
+	static GtkWidget *win;
+	if(gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(w)))
+	{
+		GtkWidget *tab;
+		GtkWidget *b;
 
-	win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_title(GTK_WINDOW(win), "Connect to a Server");
-	tab = gtk_table_new(4, 2, FALSE);
+		win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+		g_signal_connect(G_OBJECT(win), "destroy", G_CALLBACK(gtk_widget_destroyed), &win);
+		/*the gtk_widget_destroyed callback will set win to NULL*/
+		gtk_window_set_title(GTK_WINDOW(win), "Connect to a Server");
+		tab = gtk_table_new(4, 2, FALSE);
 
-	b = gtk_label_new("Server");
-	gtk_table_attach_defaults(GTK_TABLE(tab), b, 0, 1, 0, 1);
-	gtk_widget_show(b);
+		b = gtk_label_new("Server");
+		gtk_table_attach_defaults(GTK_TABLE(tab), b, 0, 1, 0, 1);
+		gtk_widget_show(b);
 
-	b = gtk_text_view_new();
-	gtk_table_attach_defaults(GTK_TABLE(tab), b, 1, 2, 0, 1);
-	gtk_widget_show(b);
+		b = gtk_text_view_new();
+		gtk_table_attach_defaults(GTK_TABLE(tab), b, 1, 2, 0, 1);
+		gtk_widget_show(b);
 
-	b = gtk_label_new("Port");
-	gtk_table_attach_defaults(GTK_TABLE(tab), b, 0, 1, 1, 2);
-	gtk_widget_show(b);
+		b = gtk_label_new("Port");
+		gtk_table_attach_defaults(GTK_TABLE(tab), b, 0, 1, 1, 2);
+		gtk_widget_show(b);
 
-	b = gtk_text_view_new();
-	gtk_table_attach_defaults(GTK_TABLE(tab), b, 1, 2, 1, 2);
-	gtk_widget_show(b);
+		b = gtk_text_view_new();
+		gtk_table_attach_defaults(GTK_TABLE(tab), b, 1, 2, 1, 2);
+		gtk_widget_show(b);
 
-	b = gtk_label_new("Username");
-	gtk_table_attach_defaults(GTK_TABLE(tab), b, 0, 1, 2, 3);
-	gtk_widget_show(b);
+		b = gtk_label_new("Username");
+		gtk_table_attach_defaults(GTK_TABLE(tab), b, 0, 1, 2, 3);
+		gtk_widget_show(b);
 
-	b = gtk_text_view_new();
-	gtk_table_attach_defaults(GTK_TABLE(tab), b, 1, 2, 2, 3);
-	gtk_widget_show(b);
+		b = gtk_text_view_new();
+		gtk_table_attach_defaults(GTK_TABLE(tab), b, 1, 2, 2, 3);
+		gtk_widget_show(b);
 
-	b = gtk_button_new_with_label("Ok");
-	/*TODO g_signal_connect(G_OBJECT(b), "clicked", G_CALLBACK(...), NULL);*/
-	gtk_table_attach_defaults(GTK_TABLE(tab), b, 0, 1, 3, 4);
-	gtk_widget_show(b);
+		b = gtk_button_new_with_label("Ok");
+		/*TODO g_signal_connect(G_OBJECT(b), "clicked", G_CALLBACK(...), NULL);*/
+		gtk_table_attach_defaults(GTK_TABLE(tab), b, 0, 1, 3, 4);
+		gtk_widget_show(b);
 
-	b = gtk_button_new_with_label("Cancel");
-	g_signal_connect_swapped(G_OBJECT(b), "clicked", G_CALLBACK(gtk_widget_destroy), G_OBJECT(win));
-	gtk_table_attach_defaults(GTK_TABLE(tab), b, 1, 2, 3, 4);
-	gtk_widget_show(b);
+		b = gtk_button_new_with_label("Cancel");
+		g_signal_connect_swapped(G_OBJECT(b), "clicked", G_CALLBACK(gtk_widget_destroy), G_OBJECT(win));
+		gtk_table_attach_defaults(GTK_TABLE(tab), b, 1, 2, 3, 4);
+		gtk_widget_show(b);
 
-	gtk_container_add(GTK_CONTAINER(win), tab);
-	gtk_widget_show(tab);
-	gtk_widget_show(win);
+		gtk_container_add(GTK_CONTAINER(win), tab);
+		gtk_widget_show(tab);
+		gtk_widget_show(win);
+	}
+	else
+	{
+		if(win != NULL)
+		{
+			gtk_widget_destroy(win);
+			win = NULL;
+		}
+	}
 }
--- src/gtk_project_new_with_sockets/quit_cb.c
+++ src/gtk_project_new_with_sockets/quit_cb.c
@@ -14,30 +14,39 @@
 */
 void quit_cb(GtkWidget *w, gpointer pWin)
 {
-	GtkWidget *win;
-	GtkWidget *tab;
-	GtkWidget *b;
+	static GtkWidget *win;
+	if(win == NULL)
+	{
+		GtkWidget *tab;
+		GtkWidget *b;
 
-	win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_title(GTK_WINDOW(win), "Quit?");
-	tab = gtk_table_new(2, 2, TRUE);
+		win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+		g_signal_connect(G_OBJECT(win), "destroy", G_CALLBACK(gtk_widget_destroyed), &win);
+		/*the gtk_widget_destroyed callback will set win to NULL*/
+		gtk_window_set_title(GTK_WINDOW(win), "Quit?");
+		tab = gtk_table_new(2, 2, TRUE);
 
-	b = gtk_label_new("Are you sure?");
-	gtk_table_attach_defaults(GTK_TABLE(tab), b, 0, 2, 0, 1);
-	gtk_widget_show(b);
+		b = gtk_label_new("Are you sure?");
+		gtk_table_attach_defaults(GTK_TABLE(tab), b, 0, 2, 0, 1);
+		gtk_widget_show(b);
 
-	b = gtk_button_new_with_label("Yes");
-	g_signal_connect_swapped(G_OBJECT(b), "clicked", G_CALLBACK(gtk_widget_destroy), G_OBJECT(pWin));
-	g_signal_connect_swapped(G_OBJECT(b), "clicked", G_CALLBACK(gtk_widget_destroy), G_OBJECT(win));
-	gtk_table_attach_defaults(GTK_TABLE(tab), b, 0, 1, 1, 2);
-	gtk_widget_show(b);
+		b = gtk_button_new_with_label("Yes");
+		g_signal_connect_swapped(G_OBJECT(b), "clicked", G_CALLBACK(gtk_widget_destroy), G_OBJECT(pWin));
+		g_signal_connect_swapped(G_OBJECT(b), "clicked", G_CALLBACK(gtk_widget_destroy), G_OBJECT(win));
+		gtk_table_attach_defaults(GTK_TABLE(tab), b, 0, 1, 1, 2);
+		gtk_widget_show(b);
 
-	b = gtk_button_new_with_label("No");
-	g_signal_connect_swapped(G_OBJECT(b), "clicked", G_CALLBACK(gtk_widget_destroy), G_OBJECT(win));
-	gtk_table_attach_defaults(GTK_TABLE(tab), b, 1, 2, 1, 2);
-	gtk_widget_show(b);
+		b = gtk_button_new_with_label("No");
+		g_signal_connect_swapped(G_OBJECT(b), "clicked", G_CALLBACK(gtk_widget_destroy), G_OBJECT(win));
+		gtk_table_attach_defaults(GTK_TABLE(tab), b, 1, 2, 1, 2);
+		gtk_widget_show(b);
 
-	gtk_container_add(GTK_CONTAINER(win), tab);
-	gtk_widget_show(tab);
-	gtk_widget_show(win);
+		gtk_container_add(GTK_CONTAINER(win), tab);
+		gtk_widget_show(tab);
+		gtk_widget_show(win);
+	}
+	else
+	{
+		gtk_window_present(GTK_WINDOW(win));
+	}
 }
